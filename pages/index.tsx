6ef03d343e82a6d4726c6d847efbfceac65628ea
vectorg3 <diglytail@gmail.com>
2023-06-17 12:24:41 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Good Deeds Home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        HomePage
+      </main>
+    </>
+  )
+}
