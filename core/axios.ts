896dbd176fecb76a22145eae00859673174d1118
vectorg3 <diglytail@gmail.com>
2023-06-19 19:39:07 +0300

    api url added to secret

--- core/axios.ts
+++ core/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { parseCookies } from 'nookies';
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_API_KEY}`;
 axios.interceptors.request.use((config) => {
     if (typeof window !== 'undefined') {
         const { token } = parseCookies();
